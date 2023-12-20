b2eef9d3185b2de45f028b4b24c1f89092a7786b
marlepo <marcinlpotrykus@gmail.com>
2023-12-20 13:58:03 +0100

    feat: balance summary widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { TimeRange } from "@const/TimeRanges";
-import { ExpenseType } from "@const/Variants";
-import { useExpenses } from "@hooks/useExpenses";
+import { TimeRange } from '@const/TimeRanges';
+import { ExpenseType } from '@const/Variants';
+import BalanceSummary from '@features/BalanceSummary';
+import { useExpenses } from '@hooks/useExpenses';
 
 export default function App() {
   const expenses = useExpenses(TimeRange.CurrentMonth);
 
   return (
-    <ul className="p-2 space-y-2 text-sm bg-white rounded-lg">
-      {expenses?.map((item) => (
-        <li key={item.id} className={item.type === ExpenseType.Earning ? "text-emerald" : "text-crayola"}>
-          ({item.type}) {item.text} <span className="font-bold">${item.amount}</span>
-        </li>
-      ))}
-    </ul>
+    <>
+      <BalanceSummary />
+      <ul className="p-2 space-y-2 text-sm bg-white rounded-lg">
+        {expenses?.map((item) => (
+          <li key={item.id} className={item.type === ExpenseType.Earning ? 'text-emerald' : 'text-crayola'}>
+            ({item.type}) {item.text} <span className="font-bold">${item.amount}</span>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
