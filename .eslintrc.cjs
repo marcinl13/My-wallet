e504391b9d5b48c861c6b08188d04914ccaa39bf
marlepo <marcinlpotrykus@gmail.com>
2023-12-20 12:15:03 +0100

    chore: eslint prettier integration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,30 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier', 'simple-import-sort'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages `react` related packages come first.
+          ['^vite', '^@vitejs', '^react', '^\\w'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Internal packages.
+          ['^(@/.*|$)'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.?(css)$']
+        ]
+      }
     ],
-  },
-}
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+  }
+};
